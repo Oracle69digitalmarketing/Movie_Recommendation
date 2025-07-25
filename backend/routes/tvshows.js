96d5cba696ac6ae9e7d1213e9d73b0f355098b51
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-07-25 07:34:48 +0100

    Update tvshows.js

--- backend/routes/tvshows.js
+++ backend/routes/tvshows.js
@@ -1,11 +1,19 @@
-// routes/tvshows.js
 const express = require("express")
-const { getPopularTVShows, getTVShowDetails, searchTVShows } = require("../controllers/tvshowsController")
+const {
+  getPopularTVShows,
+  getTVShowDetails,
+  searchTVShows,
+  getTVGenres,
+  getShowsByGenre
+} = require("../controllers/tvshowsController")
 
 const router = express.Router()
 
+// Base: /api/tvshows
 router.get("/popular", getPopularTVShows)
 router.get("/search", searchTVShows)
+router.get("/genres/list", getTVGenres)
+router.get("/genre/:genreId", getShowsByGenre)
 router.get("/:id", getTVShowDetails)
 
 module.exports = router
